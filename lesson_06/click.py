95237f31d817684771246996cac5458af3bfe8c8
KatarsisKardamon <Permakovae404@gmail.com>
2025-11-22 15:24:35 +0800

    lesson6

--- lesson_06/click.py
+++ lesson_06/click.py
@@ -1,18 +1,15 @@
 from selenium import webdriver
 from selenium.webdriver.common.by import By
-from selenium.webdriver.support.ui import WebDriverWait
-from selenium.webdriver.support import expected_conditions as EC
-
 
 driver = webdriver.Chrome()
 
-driver.get("http://uitestingplayground.com/ajax")
+driver.get("http://uitestingplayground.com/textinput")
 
-driver.find_element(By.CSS_SELECTOR, "#ajaxButton").click()
+driver.find_element(By.CSS_SELECTOR, 'input[type="text"]').send_keys('SkyPro')
 
-wait = WebDriverWait(driver, 20)
-content = wait.until(EC.visibility_of_element_located
-                     ((By.CSS_SELECTOR, "p.bg-success")))
+driver.find_element(By.CSS_SELECTOR, 'button.btn-primary').click()
+
+content = driver.find_element(By.CSS_SELECTOR, "button.btn-primary")
 
 txt = content.text
 
