c69ae767e328df8ab7a2eed0bb45d0f77300e112
KatarsisKardamon <Permakovae404@gmail.com>
2025-11-22 17:24:20 +0800

    lesson6_corrected

--- lesson_06/click.py
+++ lesson_06/click.py
@@ -1,6 +1,7 @@
 from selenium import webdriver
 from selenium.webdriver.common.by import By
-
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
 
 driver = webdriver.Chrome()
 
@@ -9,6 +10,9 @@ driver.get("http://uitestingplayground.com/textinput")
 driver.find_element(By.CSS_SELECTOR, 'input[type="text"]').send_keys('SkyPro')
 
 driver.find_element(By.CSS_SELECTOR, 'button.btn-primary').click()
+
+wait = WebDriverWait(driver, 30)
+wait.until(EC.visibility_of_element_located((By.CSS_SELECTOR, "button.btn-primary")))
 
 content = driver.find_element(By.CSS_SELECTOR, "button.btn-primary")
 
