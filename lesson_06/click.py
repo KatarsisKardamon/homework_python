42725c48ad18110d7aba3b78a9108307c75d6f02
KatarsisKardamon <Permakovae404@gmail.com>
2025-11-22 15:06:10 +0800

    lesson6

---
+++ lesson_06/click.py
@@ -0,0 +1,20 @@
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+
+driver = webdriver.Chrome()
+
+driver.get("http://uitestingplayground.com/ajax")
+
+driver.find_element(By.CSS_SELECTOR, "#ajaxButton").click()
+
+wait = WebDriverWait(driver, 20)
+content = wait.until(EC.visibility_of_element_located
+                     ((By.CSS_SELECTOR, "p.bg-success")))
+
+txt = content.text
+
+print(txt)
+
+driver.quit()
---
+++ lesson_06/picture.py
@@ -0,0 +1,18 @@
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+
+driver = webdriver.Chrome()
+
+driver.get
+('https://bonigarcia.dev/selenium-webdriver-java/loading-images.html')
+
+wait = WebDriverWait(driver, 30)
+wait.until(EC.visibility_of_element_located((By.CSS_SELECTOR, "#award")))
+
+src = driver.find_element(By.CSS_SELECTOR, '#award').get_dom_attribute("src")
+
+print(src)
+
+driver.quit()
