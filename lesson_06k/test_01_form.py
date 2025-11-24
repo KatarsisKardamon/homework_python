1f32dbec6a3ec978ab5c92643deb7cd938eaf905
KatarsisKardamon <Permakovae404@gmail.com>
2025-11-25 07:00:30 +0800

    lesson6k corrected

--- lesson_06k/test_01_form.py
+++ lesson_06k/test_01_form.py
@@ -1,66 +1,71 @@
 from selenium import webdriver
 from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
 
-driver = webdriver.Edge()
+def test_form():
+    driver = webdriver.Edge()
 
-driver.get("https://bonigarcia.dev/selenium-webdriver-java/data-types.html")
+    driver.get("https://bonigarcia.dev/selenium-webdriver-java/data-types.html")
 
-driver.find_element
-(By.CSS_SELECTOR, '[name="first-name"]').send_keys('Иван')
-driver.find_element
-(By.CSS_SELECTOR, '[name="last-name"]').send_keys('Петров')
-driver.find_element
-(By.CSS_SELECTOR, '[name="address"]').send_keys('Ленина, 55-3')
-driver.find_element
-(By.CSS_SELECTOR, '[name="e-mail"]').send_keys('test@skypro.com')
-driver.find_element
-(By.CSS_SELECTOR, '[name="phone"]').send_keys('+7985899998787')
-driver.find_element
-(By.CSS_SELECTOR, '[name="zip-code"]').send_keys('')
-driver.find_element
-(By.CSS_SELECTOR, '[name="city"]').send_keys('Москва')
-driver.find_element
-(By.CSS_SELECTOR, '[name="country"]').send_keys('Россия')
-driver.find_element
-(By.CSS_SELECTOR, '[name="job-position"]').send_keys('QA')
-driver.find_element
-(By.CSS_SELECTOR, '[name="company"]').send_keys('SkyPro')
+    driver.find_element(By.CSS_SELECTOR, '[name="first-'
+                        f'name"]').send_keys('Иван')
+    driver.find_element(By.CSS_SELECTOR, '[name="last-'
+                        f'name"]').send_keys('Петров')
+    driver.find_element(By.CSS_SELECTOR, '[name='
+                        f'"address"]').send_keys('Ленина, 55-3')
+    driver.find_element(By.CSS_SELECTOR, '[name='
+                        f'"e-mail"]').send_keys('test@skypro.com')
+    driver.find_element(By.CSS_SELECTOR, '[name='
+                        f'"phone"]').send_keys('+7985899998787')
+    driver.find_element(By.CSS_SELECTOR, '[name='
+                        f'"zip-code"]').send_keys('')
+    driver.find_element(By.CSS_SELECTOR, '[name='
+                        f'"city"]').send_keys('Москва')
+    driver.find_element(By.CSS_SELECTOR, '[name='
+                        f'"country"]').send_keys('Россия')
+    driver.find_element(By.CSS_SELECTOR, '[name='
+                        f'"job-position"]').send_keys('QA')
+    driver.find_element(By.CSS_SELECTOR, '[name='
+                        f'"company"]').send_keys('SkyPro')
 
-driver.find_element(By.CSS_SELECTOR, '[type="submit"]').click()
+    driver.find_element(By.CSS_SELECTOR, '[type="submit"]').click()
 
-background_color_red = driver.find_element(
-    By.CSS_SELECTOR, '#zip-code').value_of_css_property("background-color")
-assert (background_color_red == 'rgba(248, 215, 218, 1)')
+    background_color_red = driver.find_element(
+        By.CSS_SELECTOR, '#zip-code').value_of_css_property("background-color")
+    assert (background_color_red == 'rgba(248, 215, 218, 1)')
 
-background_color_green = driver.find_element(
-    By.CSS_SELECTOR, '#last-name').value_of_css_property("background-color")
-assert (background_color_green == 'rgba(209, 231, 221, 1)')
+    background_color_green = driver.find_element(
+        By.CSS_SELECTOR, '#last-name').value_of_css_property("background-color")
+    assert (background_color_green == 'rgba(209, 231, 221, 1)')
 
-background_color_green = driver.find_element(
-    By.CSS_SELECTOR, '#address').value_of_css_property("background-color")
-assert (background_color_green == 'rgba(209, 231, 221, 1)')
+    background_color_green = driver.find_element(
+        By.CSS_SELECTOR, '#address').value_of_css_property("background-color")
+    assert (background_color_green == 'rgba(209, 231, 221, 1)')
 
-background_color_green = driver.find_element(
-    By.CSS_SELECTOR, '#e-mail').value_of_css_property("background-color")
-assert (background_color_green == 'rgba(209, 231, 221, 1)')
+    background_color_green = driver.find_element(
+        By.CSS_SELECTOR, '#e-mail').value_of_css_property("background-color")
+    assert (background_color_green == 'rgba(209, 231, 221, 1)')
 
-background_color_green = driver.find_element(
-    By.CSS_SELECTOR, '#phone').value_of_css_property("background-color")
-assert (background_color_green == 'rgba(209, 231, 221, 1)')
+    background_color_green = driver.find_element(
+        By.CSS_SELECTOR, '#phone').value_of_css_property("background-color")
+    assert (background_color_green == 'rgba(209, 231, 221, 1)')
 
-background_color_green = driver.find_element(
-    By.CSS_SELECTOR, '#city').value_of_css_property("background-color")
-assert (background_color_green == 'rgba(209, 231, 221, 1)')
+    background_color_green = driver.find_element(
+        By.CSS_SELECTOR, '#city').value_of_css_property("background-color")
+    assert (background_color_green == 'rgba(209, 231, 221, 1)')
 
-background_color_green = driver.find_element(
-    By.CSS_SELECTOR, '#job-position').value_of_css_property("background-color")
-assert (background_color_green == 'rgba(209, 231, 221, 1)')
+    background_color_green = driver.find_element(
+        By.CSS_SELECTOR, '#job-position').value_of_css_property("background-color")
+    assert (background_color_green == 'rgba(209, 231, 221, 1)')
 
-background_color_green = driver.find_element(
-    By.CSS_SELECTOR, '#company').value_of_css_property("background-color")
-assert (background_color_green == 'rgba(209, 231, 221, 1)')
+    background_color_green = driver.find_element(
+        By.CSS_SELECTOR, '#company').value_of_css_property("background-color")
+    assert (background_color_green == 'rgba(209, 231, 221, 1)')
 
-background_color_green = driver.find_element(
-    By.CSS_SELECTOR, '#country').value_of_css_property("background-color")
-assert (background_color_green == 'rgba(209, 231, 221, 1)')
-driver.quit()
+    background_color_green = driver.find_element(
+        By.CSS_SELECTOR, '#country').value_of_css_property("background-color")
+    assert (background_color_green == 'rgba(209, 231, 221, 1)')
+
+    driver.quit()
+test_form()
--- lesson_06k/test_02_calc.py
+++ lesson_06k/test_02_calc.py
@@ -2,25 +2,28 @@ from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
+from time import sleep
 
-driver = webdriver.Chrome()
-driver.get
-("https://bonigarcia.dev/selenium-webdriver-java/slow-calculator.html")
-delay = driver.find_element(By.CSS_SELECTOR, '#delay')
-delay.clear()
-delay.send_keys(2)
-driver.find_element(By.XPATH, "//span[text()='7']").click()
-driver.find_element(By.XPATH, "//span[text()='+']").click()
-driver.find_element(By.XPATH, "//span[text()='8']").click()
-driver.find_element(By.CSS_SELECTOR, ".btn-outline-warning").click()
+def test_calculator():
+    driver = webdriver.Chrome()
+    driver.get("https://bonigarcia.dev/selenium-webdriver-java/slow-calculator.html")
+    delay = driver.find_element(By.CSS_SELECTOR, '#delay')
+    delay.clear()
+    delay.send_keys(2)
+    driver.find_element(By.XPATH, "//span[text()='7']").click()
+    driver.find_element(By.XPATH, "//span[text()='+']").click()
+    driver.find_element(By.XPATH, "//span[text()='8']").click()
+    driver.find_element(By.CSS_SELECTOR, ".btn-outline-warning").click()
 
-wait = WebDriverWait(driver, 60)
-result_element = wait.until(
-        EC.text_to_be_present_in_element((By.CSS_SELECTOR, ".screen"), "15")
-    )
-result_element = driver.find_element(By.CSS_SELECTOR, ".screen")
-result_text = result_element.text.strip()
+    wait = WebDriverWait(driver, 60)
+    result_element = wait.until(
+            EC.text_to_be_present_in_element((By.CSS_SELECTOR, ".screen"), "15")
+        )
+    result_element = driver.find_element(By.CSS_SELECTOR, ".screen")
+    result_text = result_element.text.strip()
 
-assert result_text == "15"
+    assert result_text == "15"
 
-driver.quit()
+    driver.quit()
+
+test_calculator()
